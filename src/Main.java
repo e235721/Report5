23ad9bb5377f7fe09c595aa3c1a6f7140fbee7a7
Shunto Higa <e235721@ie.u-ryukyu.ac.jp>
2023-12-12 02:25:38 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,17 @@ public class Main {
     // 条件1: String型変数 str を宣言し、初期値として null を代入する。（"null"ではない！）
     // 条件2: str.length() の内容を表示しようとする
 
-    String str = null ;
-    str.length();
+    // 条件3: 「NullPointerException」を catch せよ。
+    // 条件4: NullPointerExceptionが発生したことを標準出力せよ。出力文は、NullPointerExceptionが発生したことが分かる内容であれば自由とする。
+    // 条件5: 例外インスタンスを用いて、getMessage()を出力せよ。
 
+        try {
+            String str = null ;
+            str.length();
+
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました。" + e.getMessage()) ;
+
+        }
     }
 }
