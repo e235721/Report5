be8ce4361996fb96822ca6d91a0490baf7f2fc16
Shunto Higa <e235721@ie.u-ryukyu.ac.jp>
2023-12-12 01:58:54 +0900

    step1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    // 条件1: String型変数 str を宣言し、初期値として null を代入する。（"null"ではない！）
+    // 条件2: str.length() の内容を表示しようとする
+
+    String str = null ;
+    str.length();
+
+    }
+}
